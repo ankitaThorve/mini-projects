450f529030701cd0f5774a7cc31029611e56f38d
Ankita <ankietathorve@gmail.com>
2026-08-12 19:34:17 +0530

    feat: add current month date logic

--- 1. Custom Calendar/script.js
+++ 1. Custom Calendar/script.js
@@ -9,13 +9,25 @@ function renderCalendar() {
   const firstDay = new Date(year, month, 1).getDay();
   const lastDate = new Date(year, month + 1, 0).getDate();
 
-  console.log({ year }, { month }, { firstDay }, { lastDate });
+  // console.log({ year }, { month }, { firstDay }, { lastDate });
 
   calendarDays.innerHTML = "";
   monthYear.innerText = `${date.toLocaleString("default", {
     month: "long",
   })} ${year}`;
 
-  
+  for (let i = 0; i < firstDay; i++) {
+    calendarDays.innerHTML += '<div class="empty"></div>';
+  }
+
+  for (let d = 1; d <= lastDate; d++) {
+    const today = new Date();
+    const isToday =
+      d === today.getDate() &&
+      year === today.getFullYear() &&
+      month === today.getMonth();
+
+    calendarDays.innerHTML += `<div class="${isToday ? "today" : ""}">${d}</div>`;
+  }
 }
 renderCalendar();
